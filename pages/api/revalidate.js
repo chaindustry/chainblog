dbac78463cdb70e4175506ff62f5741f3126737d
unknown <lknsaheed@gmail.com>
2022-06-30 18:53:15 +0100

    Revalidating

---
+++ pages/api/revalidate.js
@@ -0,0 +1,10 @@
+export default async function (req, res) {
+  try {
+    await res.revalidate("/");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
