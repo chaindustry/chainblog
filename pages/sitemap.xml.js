cc3cfb772c1f98fb92123236a2065bfa7fa9fcf9
unknown <lknsaheed@gmail.com>
2023-03-07 16:36:01 +0100

    Fixed sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,7 +3,7 @@ import React from "react";
 import { baseUrl } from "../baseUrl";
 import replaceSpecChars from "../utils/replaceSpecChars";
 
-const EXTERNAL_DATA_URL = `${baseUrl}/api/posts?populate=*`;
+const EXTERNAL_DATA_URL = `${baseUrl}/api/posts?pagination[limit]=100000000&sort[0]=createdAt:desc`;
 
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
